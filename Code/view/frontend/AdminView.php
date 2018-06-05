54e46573f556d582a9a0fa590c347797cfc580d8
JUNOD Alexandre <Alexandre.JUNOD@cpnv.ch>
2018-06-05 16:54:46 +0200

    Commit 17 - suite

--- Code/view/frontend/AdminView.php
+++ Code/view/frontend/AdminView.php
@@ -1,5 +1,5 @@
 <?php ob_start(); ?>
-    <form method='post'><button type='submit' name='Settings'>Paramétrage du jeu</button></form>
+    <div class="ContainerAdminView"><form method='post'><button type='submit' name='Settings'>Paramétrage du jeu</button></form></div>
 <?php $content = ob_get_clean(); ?>
 
 <?php $Error = NULL?> <!-- Don't let the error massage come 2 times when one admin is logged -->
